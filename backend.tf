71780b04a66b15e11df21de0ace7d74b93c0524d
amanravi-kulu <aman.ravi@squareops>
2026-08-31 13:54:00 +0000

    removed s3 bucket creation

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "atmosly-terraform-testing-state"
-    key          = "vpc/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
